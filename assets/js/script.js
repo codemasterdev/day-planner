860dd6560d268420955d7cde40e894c7c1db9d1b
codemasterdev <skillz1029@gmail.com>
2021-08-27 23:32:49 -0400

    load task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
+// CURRENT hourId
+var currentHourMoment = moment().format('HH');
+
 // CURRENT DAY VARIABLE
 var currentDayMoment = moment().format("dddd MMMM Do");
 $("#currentDay").html(currentDayMoment);
+
+// LOAD TASKS FUNCTION 
+function loadTasks() {
+    // CLEAR TASK IF NO LONGER THE SAME DAY
+    if (currentHourMoment > 12) {
+        localStorage.clear();
+
+    };
+
+
+    Object.keys(localStorage).forEach((key) => {
+        $('textarea').each(function () {
+            if ($(this).attr('id') == key) {
+                $(this).val(localStorage.getItem(key));
+            }
+        });
+    });
+};
 
